811408f6a18c9091f5e8be1b6adc9ab867ee5d61
Anirudh <anirudh.a.k.haveliya1998.akh@gmail.com>
2019-10-29 02:02:11 +0530

    Adding Java program to sort an array.

---
+++ JAVA/ArraysSort.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class ArraysSort
+{
+    public static void main(String args[])
+    {
+        int array[]={5,6,7,3,4,2,8,1,9};
+        Arrays.sort(array);
+            System.out.print(Arrays.toString(array));
+    }
+}
